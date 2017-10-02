abfd309e64e0ba3de2d6ff66d822b4a73744e8be
sahin <electrocoder@gmail.com>
2017-10-02 22:50:56 +0300

    django project

---
+++ manisaotomasyon/urls.py
@@ -0,0 +1,48 @@
+"""manisaotomasyon URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.views.static import serve
+from django.views.generic import RedirectView
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps.views import sitemap
+
+from blog import views as blog
+
+import settings
+
+urlpatterns = [
+    # front page index page
+    url(r'^$', blog.index_front, name='index_front'),
+
+    url(r'^admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT,}),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
